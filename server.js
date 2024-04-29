adcd3c2c2cd752c834d59b06de8b3750816bb530
Nikol-Papismadov <nikol6516@gmail.com>
2024-04-29 18:38:40 +0300

    new1

--- server.js
+++ server.js
@@ -1,70 +1,71 @@
 import express from "express";
+import cors from "cors";
 import http from "http";
 import { Server as socketIO } from "socket.io";
-import{dirname} from "path";
+import { dirname } from "path";
 import path from "path";
 import { fileURLToPath } from 'url';
 
+const PORT = process.env.PORT || 3000;
 
-const PORT=process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __diename = dirname(__filename);
 
-const __filename=fileURLToPath(import.meta.url);
-const __diename=dirname(__filename);
+const app = express();
+app.use(cors());
 
-const app=express();
-const server=http.createServer(app);
-const io=new socketIO(server,{
-    cors:{
-        origin:[
-            "http://localhost:3000",
-            "https://localhost:5173",
+const server = http.createServer(app);
+
+const io = new socketIO(server, {
+    cors: {
+        origin: [
+            "http://localhost:5173",
         ],
-        methods:["GET","POST"]
+        methods: ["GET", "POST"]
     }
 });
 
-app.use(express.static(path.join(__diename,"public")))
-const users={};
+app.use(express.static(path.join(__diename, "public")));
+const users = {};
 
-
-io.on("connection",(socket)=>{
-    socket.on("join",(username)=>{
-        users[socket.id]=username;
-        io.emit("userJoined",{username,userId: socket.id});
-        console.log(`server:${username} Joined the chat`)
+io.on("connection", (socket) => {
+    socket.on("join", (username) => {
+        users[socket.id] = username;
+        io.emit("userJoined", { username, userId: socket.id });
+        console.log(`server:${username} Joined the chat`);
+        
     });
-    socket.on("disconnect",()=>{
-        const username=users[socket.id];
+    socket.on("disconnect", () => {
+        const username = users[socket.id];
         delete users[socket.id];
-        io.emit("userLeft",{username,userId:socket.id});
-        console.log(`SERVER:${username} left the chat`)
+        io.emit("userLeft", { username, userId: socket.id }); // Include username here
+        console.log(`SERVER:${username} left the chat`);
     });
-    socket.on("ChatMessage",(message)=>{
-        const username=users[socket.id];
-        io.emit("message",{username,message});
-        console.log(`${username}: ${message}`)
+    
+    socket.on("ChatMessage", (message) => {
+        const username = users[socket.id];
+        io.emit("message", { username, message });
+        console.log(`${username}: ${message}`);
     });
-    socket.on("privateMessage",({recipientId,message})=>{
-        const senderUsername=users[socket.id];
-        const recipientSocket=io.sockets.sockets.get(recipientId);
-        if(recipientSocket){
-            recipientSocket.emit("privateMessage",{senderUsername,message});
-        } 
+    socket.on("privateMessage", ({ recipientId, message }) => {
+        const senderUsername = users[socket.id];
+        const recipientSocket = io.sockets.sockets.get(recipientId);
+        if (recipientSocket) {
+            recipientSocket.emit("privateMessage", { senderUsername, message });
+        }
     });
-    socket.on("getAllUsers",()=>{
-        const usersList=Object.values(users);
-        socket.emit("allUsers",usersList);
-        const message=`List of all users: ${usersList.join(", ")}`;
-        socket.emit("PrivateMessage",{senderUsername:"SERVER",message});
+    socket.on("getAllUsers", () => {
+        const usersList = Object.values(users);
+        socket.emit("allUsers", usersList);
+        const message = `List of all users: ${usersList.join(", ")}`;
+        socket.emit("PrivateMessage", { senderUsername: "SERVER", message });
     });
 });
 
-
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Welcome to our app");
-})
+});
 
-
-server.listen(PORT,()=>{
+server.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
-})
+});
