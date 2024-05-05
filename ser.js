4efe0347b514730d8d790288262e299af0a0f404
Nikol-Papismadov <nikol6516@gmail.com>
2024-05-05 15:09:48 +0300

    new4

--- ser.js
+++ ser.js
@@ -9,7 +9,7 @@ import { fileURLToPath } from 'url';
 const PORT = process.env.PORT || 3001;
 
 const __filename = fileURLToPath(import.meta.url);
-const __diename = dirname(__filename);
+const _diename = dirname(_filename);
 
 const app = express();
 app.use(cors());
@@ -27,33 +27,32 @@ const io = new socketIO(server, {
 });
 
 app.use(express.static(path.join(__diename, "public")));
+
 const activeUsers = {};
 const offlineUsers = {};
+// let users = [];
 io.on("connection", (socket) => {
     socket.on("join", (username,accessToken,refreshToken) => {
         activeUsers[socket.id] = username;
         io.emit("activeUsers", Object.values(activeUsers));
-        io.emit("offlineUsers", Object.values(offlineUsers));
         // users.push({ id: socket.id, username:username, accessToken:accessToken, refreshToken:refreshToken  });
         socket.broadcast.emit("userJoined", { userId: socket.id,username,accessToken:accessToken, refreshToken:refreshToken });
-        console.log(`server:${username} Joined the chat`);
+        console.log(server:${username} Joined the chat);
         
     });
     socket.on("disconnect", () => {
-        
         const username = activeUsers[socket.id];
         delete activeUsers[socket.id];
         io.emit("activeUsers", Object.values(activeUsers));
-        io.emit("offlineUsers", Object.values(offlineUsers));
 
         socket.broadcast.emit("userLeft", { username, userId: socket.id }); // Include username here
-        console.log(`SERVER:${username} left the chat`);
+        console.log(SERVER:${username} left the chat);
     });
     
     socket.on("ChatMessage", (message) => {
         const username = activeUsers[socket.id];
         io.emit("message", { username, message });
-        console.log(`${username}: ${message}`);
+        console.log(${username}: ${message});
     });
     // socket.on("privateMessage", ({ recipientId, message }) => {
     //     const senderUsername = users[socket.id];
@@ -66,7 +65,7 @@ io.on("connection", (socket) => {
     // socket.on("getAllUsers", () => {
     //     const usersList = Object.values(users);
     //     socket.emit("allUsers", usersList);
-    //     const message = `List of all users: ${usersList.join(", ")}`;
+    //     const message = List of all users: ${usersList.join(", ")};
     //     socket.emit("PrivateMessage", { senderUsername: "SERVER", message });
     // });
 });
@@ -75,10 +74,10 @@ app.get("/", (req, res) => {
     res.send("Welcome to our app");
 });
 
-app.get("/online-users", (req, res) => {
-    res.json(Object.values(activeUsers));
-});
+// app.get("/online-users", (req, res) => {
+//     res.json(Object.values(activeUsers));
+// });
 
 server.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`);
+    console.log(server running on port ${PORT});
 });
